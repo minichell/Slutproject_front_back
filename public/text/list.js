dbcb790f01b3d95ab6a4b3e1b9b070a5f607e489
minichell <dorna-michell.dadelahi@elev.ga.ntig.se>
2021-05-09 11:50:30 +0200

    fixade todo

--- public/text/list.js
+++ public/text/list.js
@@ -1,27 +1,26 @@
-  
 const app = new Vue ({
     el: '#app',
     data: {
-        title: 'Hellow Coding',
+        title: 'Hello Coding Garden',
         newTodo: '',
         todos: []
     },
-    methods:{
+    methods: {
         addTodo() {
-           this.todos.push({
-               title: this.newTodo,
-               done: false
-           });
-           this.newTodo = '';
+            this.todos.push({
+                title: this.newTodo,
+                done: false
+            });
+            this.newTodo = '';
         },
-        removeTodo(todo){
+        removeTodo(todo) {
             const todoIndex = this.todos.indexOf(todo);
             this.todos.splice(todoIndex, 1);
         },
         allDone(){
-            this.todos.forEach(todo =>{
+            this.todos.forEach(todo => {
                 todo.done = true;
-            });
+            })
         }
     }
-    });
+});
